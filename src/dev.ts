c435f5f01ef5dd953cec41b3501f3cab3ffc5cba
phamthainb <phamthai4643@gmail.com>
2023-12-09 16:22:23 +0700

    [master] validate url

--- src/dev.ts
+++ src/dev.ts
@@ -2,10 +2,12 @@ import { join } from "path";
 import { largeFileDownloader } from ".";
 
 // values
+const vid10MB = `https://github.com/phamthainb/large-file-downloader/raw/master/sample/10MB.mp4`;
+const vid18MB = `https://github.com/phamthainb/large-file-downloader/raw/master/sample/18MB.mp4`;
 
 (async () => {
   const out = await largeFileDownloader({
-    fileUrl: ``,
+    fileUrl: vid10MB,
     chunkSizeInBytes: 50 * 1024 * 1024,
     logger: true,
     destinationFolder: join(__dirname, "temp"),
